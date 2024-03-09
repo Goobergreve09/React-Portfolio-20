06fda0f2b5ce7a0576cfd65b74ff471c2a75d564
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-09 06:00:16 -0500

    netflify contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -63,7 +63,7 @@ export default function Contact() {
       <Header />
       <div className="container pt-4 contact">
         <h2>Contact Me</h2>
-        <form onSubmit={handleSubmit} name="contact">
+        <form onSubmit={handleSubmit} name="contact" netlify>
           <input type="hidden" name="form-name" value="contact" />
           <div className="mb-3">
             <label htmlFor="name" className="form-label">
@@ -123,5 +123,6 @@ export default function Contact() {
     </div>
   );
 }
+
 
 
