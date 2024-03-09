49e49735c19c843a72ff8c948504ac73153c2b97
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-09 06:24:13 -0500

    netlify contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -52,7 +52,7 @@ export default function Contact() {
       <Header />
       <div className="container pt-4 contact">
         <h2>Contact Me</h2>
-        <form onSubmit={handleSubmit} name="contact" method="POST" data-netlify="true">
+        <form onSubmit={handleSubmit} name="gwegawergwe" method="POST" data-netlify="true">
           <div className="mb-3">
             <label htmlFor="name" className="form-label">
               Name
