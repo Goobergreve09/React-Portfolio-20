23680e657e721343e2c99c76f6934b5633690cbb
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-13 04:20:40 -0400

    changed contat structure for emailjs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import { useState } from "react";
-import { send } from 'emailjs-com';
+import { send } from "emailjs-com";
 
 export default function Contact() {
   const [formData, setFormData] = useState({
-    name: "",
+    firstName: "",
+    lastName: "",
     email: "",
+    company: "",
     message: "",
   });
   const [errors, setErrors] = useState({});
@@ -17,8 +19,11 @@ export default function Contact() {
 
   const validateForm = () => {
     const errors = {};
-    if (!formData.name.trim()) {
-      errors.name = "Name is required";
+    if (!formData.firstName.trim()) {
+      errors.firstName = "First Name is required";
+    }
+    if (!formData.lastName.trim()) {
+      errors.lastName = "Last Name is required";
     }
     if (!formData.email.trim()) {
       errors.email = "Email is required";
@@ -41,14 +46,20 @@ export default function Contact() {
     e.preventDefault();
     if (validateForm()) {
       // Send email using EmailJS
-      send('service_18mue07', 'template_cfjw3yl', formData, 'nkbfmoPkd4Tr3OQNX')
+      send("service_18mue07", "template_cfjw3yl", formData, "nkbfmoPkd4Tr3OQNX")
         .then((response) => {
-          console.log('Email sent successfully:', response);
+          console.log("Email sent successfully:", response);
           setSuccessMessage("Message sent successfully!");
-          setFormData({ name: "", email: "", message: "" });
+          setFormData({
+            firstName: "",
+            lastName: "",
+            email: "",
+            company: "",
+            message: "",
+          });
         })
         .catch((error) => {
-          console.error('Error sending email:', error);
+          console.error("Error sending email:", error);
           setSuccessMessage("");
           // You can set an error message here if needed
         });
@@ -60,38 +71,79 @@ export default function Contact() {
       <Header />
       <div className="container pt-4 contact">
         <h2>Contact Me</h2>
+        <h4 className="text-center  mb-5">
+          If you'd like to contact me about opportunities or to work on projects
+          together, please fill out the form below:
+        </h4>
         <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              className={`form-control ${errors.name ? "is-invalid" : ""}`}
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-            {errors.name && (
-              <div className="invalid-feedback">{errors.name}</div>
-            )}
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <label htmlFor="firstName" className="form-label">
+                First Name
+              </label>
+              <input
+                type="text"
+                className={`form-control ${
+                  errors.firstName ? "is-invalid" : ""
+                }`}
+                id="firstName"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+              />
+              {errors.firstName && (
+                <div className="invalid-feedback">{errors.firstName}</div>
+              )}
+            </div>
+            <div className="col-md-6">
+              <label htmlFor="lastName" className="form-label">
+                Last Name
+              </label>
+              <input
+                type="text"
+                className={`form-control ${
+                  errors.lastName ? "is-invalid" : ""
+                }`}
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+              {errors.lastName && (
+                <div className="invalid-feedback">{errors.lastName}</div>
+              )}
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className={`form-control ${errors.email ? "is-invalid" : ""}`}
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            {errors.email && (
-              <div className="invalid-feedback">{errors.email}</div>
-            )}
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className={`form-control ${errors.email ? "is-invalid" : ""}`}
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              {errors.email && (
+                <div className="invalid-feedback">{errors.email}</div>
+              )}
+            </div>
+            <div className="col-md-6">
+              <label htmlFor="company" className="form-label">
+                Company
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="company"
+                name="company"
+                value={formData.company}
+                onChange={handleChange}
+              />
+            </div>
           </div>
           <div className="mb-3">
             <label htmlFor="message" className="form-label">
