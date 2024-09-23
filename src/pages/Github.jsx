c0527109765aeec2ef3f5616343c04eddd6668f2
Goobergreve09 <gregory.greve@yahoo.com>
2024-09-23 01:48:53 -0400

    added badges

--- src/pages/Github.jsx
+++ src/pages/Github.jsx
@@ -151,6 +151,7 @@ function GitHub() {
                     <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
                     <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
                     <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
+                    <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=fff" />
                   </Col>
                   <Col xs={12} sm={6} md={4} className="text-center p-3">
                     <h6>Frameworks</h6>
@@ -175,6 +176,7 @@ function GitHub() {
                   </Col>
                   <Col xs={12} sm={6} md={4} className="text-center p-3">
                     <h6>IDE</h6>
+                    <img src="https://img.shields.io/badge/PyCharm-000?style=for-the-badge&logo=pycharm&logoColor=fff)" />
                     <img src="https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white" />
                   </Col>
                   <Col xs={12} sm={6} md={4} className="text-center p-3">
