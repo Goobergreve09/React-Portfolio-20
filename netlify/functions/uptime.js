b709c4dfadaa61653efd62acc8df1d2326823cd0
Goobergreve09 <gregory.greve@yahoo.com>
2024-09-23 07:05:54 -0400

    netlify function

--- netlify/functions/uptime.js
+++ netlify/functions/uptime.js
@@ -1,34 +1,37 @@
-import fetch from "node-fetch"; // Use import instead of require
-
 export async function handler(event, context) {
-  const apiKey = process.env.API_KEY; // Ensure this is set in your environment variables
-  console.log("Function invoked");
-
-  try {
-    const response = await fetch("https://api.uptimerobot.com/v2/getMonitors", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        api_key: apiKey,
-        format: "json",
-        response_times: "1",
-      }),
-    });
-
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Error fetching uptime data" }),
-    };
+    const apiKey = process.env.API_KEY;
+  
+    try {
+      const response = await fetch("https://api.uptimerobot.com/v2/getMonitors", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          api_key: apiKey,
+          format: "json",
+          response_times: "1",
+        }),
+      });
+  
+      const data = await response.json();
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST',
+        },
+        body: JSON.stringify(data),
+      };
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: "Error fetching uptime data" }),
+      };
+    }
   }
-}
+  
+  
 
 
